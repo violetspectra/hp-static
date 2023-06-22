0c4b429006a0dfe822dd794c248ca174859dd3e8
Violet Spectra <spectralpegasus@gmail.com>
2023-06-22 15:55:45 +1200

    Add motivational quote

--- pages/index.vue
+++ pages/index.vue
@@ -4,7 +4,42 @@ useHead({});
 <template>
   <div class="text-center text-white py-5">
     <h1>Hypnoponies</h1>
-    <p>Welcome to Hypnoponies</p>
-    <a href="https://discord.gg/WBVkUTN" class="text-white">Join the Discord</a>
+  <div class="w-100 py-5 bg-gradient-darkpurple-lessdarkpurple">
+    <div class="d-flex justify-content-center">
+      <div
+        class="text-white rounded-3 px-3 py-3"
+        style="background-color: #ffffff38; max-width: 40rem"
+      >
+        <figure class="mb-0 text-start">
+          <blockquote class="blockquote fs-6 mb-4">
+            <p>
+              "Like the ever-flowing magic of friendship, our journey through
+              life is filled with moments of change and growth. Just as the sun
+              rises each day, casting away the darkness, we too must embrace the
+              dawn of progression. As we bid farewell to the past, let us
+              cherish its lessons, for they shape the path to a brighter future.
+              With each step forward, we become stronger, wiser, and kinder,
+              evolving into the ponies we are destined to be. So spread your
+              wings and let the winds of change carry you to new heights, for
+              the magic of friendship will guide you, always.""
+            </p>
+          </blockquote>
+          <figcaption class="blockquote-footer text-white mb-0">
+            With love, <cite title="Source Title">Rainbow Dash ♥</cite>
+          </figcaption>
+        </figure>
+      </div>
+    </div>
   </div>
 </template>
+
+<style scoped>
+.bg-gradient-darkpurple-lessdarkpurple {
+  background: rgb(19, 11, 26);
+  background: linear-gradient(
+    27deg,
+    rgba(19, 11, 26, 1) 0%,
+    rgba(64, 41, 84, 1) 100%
+  );
+}
+</style>
