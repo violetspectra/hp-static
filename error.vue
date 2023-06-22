bbb3e1c6dd9d16dddb387bf85b9bbc64a0d67d61
Violet Spectra <spectralpegasus@gmail.com>
2023-06-22 17:14:33 +1200

    Add error page

---
+++ error.vue
@@ -0,0 +1,20 @@
+<script setup>
+const props = defineProps({
+  error: Object,
+});
+
+const handleError = () => clearError({ redirect: "/" });
+</script>
+
+<template>
+  <NuxtLayout>
+    <div class="container py-5 text-center text-white">
+      <h1>{{ error.statusCode }}</h1>
+      <p v-if="error.statusCode === 404">Page not found</p>
+      <p v-else>An error occurred...</p>
+      <a @click="handleError" class="text-white" style="cursor: pointer"
+        >&lt; Go back home</a
+      >
+    </div>
+  </NuxtLayout>
+</template>
