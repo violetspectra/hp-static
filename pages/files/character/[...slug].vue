f682c1ad7970ba21e8a65981a28eb54eaec9a727
Violet Spectra <spectralpegasus@gmail.com>
2023-06-23 00:48:42 +1200

    Move warnings into page template

--- pages/files/character/[...slug].vue
+++ pages/files/character/[...slug].vue
@@ -39,6 +39,39 @@ if (!data.value) {
         <div
           class="rounded px-3 py-4 prose-container transparent-white-background"
         >
+          <p class="h4">General Information</p>
+          <p>
+            Before even starting pony hypnosis, please read over everything in
+            this thread here. After doing that, really think over it all and be
+            100% sure you know what you're getting into. Hypnosis is not a toy
+            nor a game. We want ponies to enjoy these files, but we also want
+            them to be aware. All these warnings and disclaimers may be scary,
+            but they're here for a few reasons. They're here to make you aware
+            of what you're getting into. Also to make sure you go about pony
+            hypnosis in the safest way possible. I want to give a quick thank
+            you to Star Shard for coming up with the idea for this new style. I
+            also want to give a quick thank you to Bright Star for coming up
+            with the format of the download section of this thread.
+          </p>
+          <p class="h4">Emotional Kernel Panic Warning</p>
+          <p>
+            EKP, which is short for Emotional Kernel Panic, is something very
+            serious and not to be taken lightly. It's when the effects of one
+            hypnosis file attempt to overwrite another hypnosis file, causing an
+            identity crisis or crisis of desire. It can be anywhere from
+            annoying, to painfully mind wrenching. It's avoided by not switching
+            between files. You are however free to use two different files of
+            the same pony. So for example you could use version 2.0 and version
+            3.0 of this file without any trouble. So in short, do NOT use two
+            different pony hypnosis files unless they are the same pony, but
+            different versions. If you really must switch files for whatever
+            reason, you can, but only if you use the "Pony TF RESET" file. It
+            can be found under the support files here. Just be sure to use it
+            multiple times before starting up a new file. Also be aware that
+            there have been reports of ponies getting EKP even after resetting.
+            So it's better to really think through your choice of which pony you
+            want to use and stick with them.
+          </p>
           <ContentRenderer :value="data?.body">
             <ContentRendererMarkdown :value="data?.body" />
           </ContentRenderer>
