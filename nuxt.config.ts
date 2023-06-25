3bb27b0161d31310d5e63b847a5f351b12ce1460
Violet Spectra <spectralpegasus@gmail.com>
2023-06-25 22:25:46 +1200

    Remove config changes and alter nitro to delete 200.html

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,21 +1,21 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: ["@nuxt/content"],
-  devtools: { enabled: true },
+  // devtools: { enabled: true },
   css: [
     "~/assets/style/theme.scss",
     "~/node_modules/bootstrap-icons/font/bootstrap-icons.min.css",
   ],
-  // content: {
-  // documentDriven: {
-  //   injectPage: false,
-  // },
-  // },
-  ssr: true,
-  // router: {
-  //   options: {
-  //     strict: false,
-  //   },
-  // },
-  // sourcemap: false,
+  content: {
+    documentDriven: {
+      injectPage: false,
+    },
+  },
+  hooks: {
+    "nitro:config"(config) {
+      config.prerender!.routes = config.prerender!.routes!.filter(
+        (r) => r !== "/200.html"
+      );
+    },
+  },
 });
