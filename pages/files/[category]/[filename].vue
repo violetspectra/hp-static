749e0e9dd3427e505190cedcd8ba48cb1df40b49
Violet Spectra <spectralpegasus@gmail.com>
2023-06-25 21:32:10 +1200

    Enable fatal error

--- pages/files/[category]/[filename].vue
+++ pages/files/[category]/[filename].vue
@@ -15,7 +15,11 @@ interface HypnoFile extends ParsedContent {
 
 const data = await queryContent<HypnoFile>(path).findOne();
 if (!data) {
-  throw createError({ statusCode: 404, statusMessage: "Page not found" });
+  throw createError({
+    statusCode: 404,
+    statusMessage: "Page not found",
+    fatal: true,
+  });
 }
 
 /**
